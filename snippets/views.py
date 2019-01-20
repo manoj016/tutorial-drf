3fa1746a451c7c58857393abbbd0b10e7cdf55c9
manoj016 <manoj@quartic.ai>
2019-01-20 12:49:00 +0530

    Added format

--- snippets/views.py
+++ snippets/views.py
@@ -9,7 +9,7 @@ from rest_framework.decorators import api_view
 
 
 @api_view(['GET', 'POST'])
-def snippet_list(request):
+def snippet_list(request, format=None):
     """
     List all snippets, or create a new snippet
     """
@@ -26,7 +26,7 @@ def snippet_list(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'PUT', 'DELETE'])
-def snippet_detail(request, pk):
+def snippet_detail(request, pk, format=None):
     """
     Retrieve, update or delete a snippet
     :param request:
